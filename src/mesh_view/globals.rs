88e215919e861c372d5d8e06042d5de2d5575e34
Josh Palmer <1253239+Shfty@users.noreply.github.com>
2023-03-03 23:32:05 +0000

    Split Globals into Globals and GlobalsWasm to account for alignment differences

--- src/mesh_view/globals.rs
+++ src/mesh_view/globals.rs
@@ -1,13 +1,17 @@
-pub trait WasmPadding {}
-
-/// No WASM padding
-impl WasmPadding for () {}
-
-/// Single float WASM padding
-impl WasmPadding for f32 {}
+#[repr(C)]
+pub struct Globals {
+    // The time since startup in seconds
+    // Wraps to 0 after 1 hour.
+    pub time: f32,
+    // The delta time since the previous frame in seconds
+    pub delta_time: f32,
+    // Frame count since the start of the app.
+    // It wraps to zero when it reaches the maximum value of a u32.
+    pub frame_count: u32,
+}
 
 #[repr(C)]
-pub struct Globals<P: WasmPadding = ()> {
+pub struct GlobalsWasm {
     // The time since startup in seconds
     // Wraps to 0 after 1 hour.
     pub time: f32,
@@ -18,5 +22,5 @@ pub struct Globals<P: WasmPadding = ()> {
     pub frame_count: u32,
 
     // WebGL2 structs must be 16 byte aligned.
-    _wasm_padding: P,
+    _wasm_padding: f32,
 }
